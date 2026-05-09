3bcf5ac3791327ed76c59153c30055088fb490c6
Cauã Balzaneli <cauabalzaneli81@gmail.com>
2026-05-08 21:14:14 -0300

    add API + PRISMA

--- server.js
+++ server.js
@@ -1,3 +1,22 @@
-import express from 'express'
+import express, { request } from 'express'
 
 const app = express()
+app.use(express.json())
+
+
+app.post('/usuarios', (request, response) => {
+
+
+    users.push(request.body)
+
+    
+    response.status(201).json(request.body)
+})
+
+
+app.get('/usuarios', (request, response) => {
+    response.status(200).json(users)
+})
+
+
+app.listen(3000)
