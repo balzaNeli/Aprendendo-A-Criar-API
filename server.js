27bc1d0b735f64d5ed32d8d15a5ada42f9cefdde
Cauã Balzaneli <cauabalzaneli81@gmail.com>
2026-05-08 22:47:08 -0300

    upd API

--- server.js
+++ server.js
@@ -25,11 +25,50 @@ app.post('/usuarios', async (request, response) => {
 
 app.get('/usuarios', async (request, response) => {
     try {
-        const users = await prisma.user.findMany() 
+        let users = []
+        if(request.query){
+            users = await prisma.user.findMany({
+                where: {
+                    name: request.query.name,
+                    email: request.query.email,
+                    age: request.query.age,
+                    id: request.query.id
+
+                }
+            })
+        }else{
+           users = await prisma.user.findMany() 
+        }
+        
         response.status(200).json(users)
     } catch (error) {
         response.status(500).json({ error: error.message })
     }
 })
+app.put('/usuarios/:id', async (request, response) => {
+    try {
+        const user = await prisma.user.update({
+            where: { id: request.params.id },
+            data: {
+                email: request.body.email,
+                name: request.body.name,
+                age: request.body.age
+            }
+        })
+        response.status(200).json(user)
+    } catch (error) {
+        response.status(500).json({ error: error.message })
+    }
+})
 
+app.delete('/usuarios/:id', async (request, response) => {
+    try {
+        await prisma.user.delete({
+            where: { id: request.params.id }
+        })
+        response.status(200).json({ message: "Usuário deletado com sucesso!" })
+    } catch (error) {
+        response.status(500).json({ error: error.message })
+    }
+})
 app.listen(3000)
